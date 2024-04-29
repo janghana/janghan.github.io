1707ae25dc227e4f1391bc3521a7fbc122e67592
janghana <han001112@icloud.com>
2024-04-29 20:29:34 +0900

    upload react files

---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Feel free to reach out to me at:</p>
+      <ul>
+        <li>Email: [your-email@example.com]</li>
+        <li>LinkedIn: [LinkedIn Profile]</li>
+        <li>Twitter: [@yourhandle]</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contact;
